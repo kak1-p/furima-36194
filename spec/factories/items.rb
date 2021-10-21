a94585fdb061627a0d63aa6f2ced75d5a73b0738
kak1-p <kak1.ppp456@gmail.com>
2021-10-21 16:40:05 +0900

    itemモデルテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    items_name  {Faker::Lorem.sentence}
+    detail      {Faker::Lorem.sentence}
+    category_id {'2'}
+    status_id   {'2'}
+    fee_id      {'2'}
+    city_id     {'2'}
+    delivery_date_id  {'2'}
+    price       {'555'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
