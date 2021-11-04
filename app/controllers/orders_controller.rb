5ad8ff297539753ae53d3b25902709f594e16808
kak1-p <kak1.ppp456@gmail.com>
2021-11-04 12:42:06 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,10 +8,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  def new
-    @order_address = OrderAddress.new
-  end
-
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
